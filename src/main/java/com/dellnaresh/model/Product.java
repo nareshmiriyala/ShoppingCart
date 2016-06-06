c169e34f49f862ee1616f9e3a428ce15e4e62112
Administrator <admin@example.com>
2016-06-06 21:09:59 +1000

    Adding Prouct model

---
+++ src/main/java/com/dellnaresh/model/Product.java
@@ -0,0 +1,65 @@
+package com.dellnaresh.model;
+
+import org.hibernate.search.annotations.Field;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+/**
+ * Created by nareshm on 6/6/2016.
+ */
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Field
+    @NotNull
+    private String name;
+    @Field
+    @NotNull
+    private String image;
+    @Field
+    @NotNull
+    private BigDecimal unitPrice;
+
+    public Product(String name, String image, BigDecimal unitPrice) {
+        this.name = name;
+        this.image = image;
+        this.unitPrice = unitPrice;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
